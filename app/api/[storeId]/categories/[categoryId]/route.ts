50b0fbc0c22879781db6409b20ad3c09341c0da2
Phahladira-Moja <phahladira@patala.co.za>
2023-07-27 15:48:19 +0200

    Added the categories view /tab
    - Added the API routes to affect the create, patch, and get categories
    - added the categories schema/model
    - added the select shadcnui component

---
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -0,0 +1,129 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    if (!params.categoryId) {
+      return new NextResponse("Category ID is required", { status: 400 });
+    }
+
+    const category = await prismadb.category.findUnique({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORIES_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, billboardId } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse("Billboard Id is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    if (!params.categoryId) {
+      return new NextResponse("Category ID is required", { status: 400 });
+    }
+
+    // we are checking that the store belongs to the user
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("unauthorized", { status: 403 });
+    }
+
+    const category = await prismadb.category.updateMany({
+      where: {
+        id: params.categoryId,
+      },
+      data: {
+        name,
+        billboardId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORIES_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request, // we need this even though it's not used. the params is only in second argument
+  { params }: { params: { storeId: string; categoryId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    if (!params.categoryId) {
+      return new NextResponse("Category ID is required", { status: 400 });
+    }
+
+    // we are checking that the store belongs to the user
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("unauthorized", { status: 403 });
+    }
+
+    const category = await prismadb.category.deleteMany({
+      where: {
+        id: params.categoryId,
+      },
+    });
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORY_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
